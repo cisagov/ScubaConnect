f34122e059f47259293789844ac9902ee2c91864
Elijah Agbayani <eagbaya@sandia.gov>
2025-06-02 11:23:48 -0400

    Pass in the SAS variable

--- m365/terraform/main.tf
+++ m365/terraform/main.tf
@@ -67,6 +67,7 @@ module "container" {
   subnet_ids                   = var.vnet == null ? null : [module.networking[0].aci_subnet_id]
   schedule_interval            = var.schedule_interval
   output_storage_container_url = var.output_storage_container_url
+  output_storage_container_sas = var.output_storage_container_sas
   input_storage_container_url  = var.input_storage_container_url
   contact_emails               = var.contact_emails
   log_analytics_workspace      = azurerm_log_analytics_workspace.monitor_law
