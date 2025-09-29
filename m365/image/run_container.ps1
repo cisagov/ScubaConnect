fac507cf38b7038c13e461ecbc6899a5af92bf06
jacdavi <86626873+jacdavi@users.noreply.github.com>
2025-09-29 16:13:48 -0700

    adjust paths for cleaner output without duplicates

--- m365/image/run_container.ps1
+++ m365/image/run_container.ps1
@@ -92,8 +92,8 @@ Foreach ($tenantConfig in $(Get-ChildItem 'input\')) {
         Write-Output "  Starting Upload"
         $DatePath = Get-Date -Format "yyyy/MM/dd"
         if ("true" -eq $Env:OUTPUT_ALL_FILES) {
-            $InPath = ".\reports\$($org)"
-            $OutPath = "$($Env:REPORT_OUTPUT)/$($DatePath)/$($org)"
+            $InPath = ".\reports\$($org)\*"
+            $OutPath = "$($Env:REPORT_OUTPUT)/$($DatePath)/$($org)-$([int]$(Get-Date).TimeOfDay.TotalSeconds)"
         }
         else {
             $ResultsFile.FullName
