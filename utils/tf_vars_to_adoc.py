175f4b00a446f1c806517c32b3f8518c495cd49b
Jacob <86626873+jacdavi@users.noreply.github.com>
2025-12-08 12:18:25 -0800

    feat(util): parse section header comments when parsing terraform to adoc (#22)
    update m365 adoc with missing variables

--- utils/tf_vars_to_adoc.py
+++ utils/tf_vars_to_adoc.py
@@ -1,16 +1,24 @@
 import hcl2
 import argparse
+import re
 
-parser = argparse.ArgumentParser("Converts variables in a .tf file to asciidoc description list")
+COMMENT_HEADER_REGEX = r'###\s*(.*?)\s*#*\s*variable\s*"(.*)"'
+
+parser = argparse.ArgumentParser("Converts a variables.tf file to an asciidoc description list. Treats comment blocks starting with ### as section headers")
 parser.add_argument("variables_tf")
 args = parser.parse_args()
 
-
 with open(args.variables_tf, 'r') as f:
     d = hcl2.load(f)
+    f.seek(0)
+    text = '\n'.join(f.readlines())
+    category_matches = re.findall(COMMENT_HEADER_REGEX, text, flags=re.MULTILINE)
+    first_var_to_cat = { m[1]: m[0] for m in category_matches }
 
 for v in d['variable']:
     name = list(v.keys())[0]
+    if name in first_var_to_cat:
+        print(f"{first_var_to_cat[name].title()}::")
     t = v[name]['type'].strip("{}$")
     if t.startswith("object"):
         t = "object"
