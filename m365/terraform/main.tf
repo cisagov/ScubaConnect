d5adb52c3f3c5a1a78156c2f2b92e4ea08e03110
jacdavi <86626873+jacdavi@users.noreply.github.com>
2026-03-30 11:52:54 -0700

    fix(m365): explicitly enable local auth for log analytics workspace
    this fixes an issue where container logs were not appearing and was caused by an issue in the terraform provider setting the default incorrectly

--- m365/terraform/main.tf
+++ m365/terraform/main.tf
@@ -21,6 +21,7 @@ resource "azurerm_log_analytics_workspace" "monitor_law" {
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
   retention_in_days   = 90
+  local_authentication_enabled = true
 
   lifecycle {
     ignore_changes = [tags]
